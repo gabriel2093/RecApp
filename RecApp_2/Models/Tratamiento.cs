01712ada452a4fe399d100397482bda325f58d62
Gabriel Jiménez <gjimenez.com@gmail.com>
2016-09-11 16:46:04 -0600

    Mantenimiento de tratamientos

---
+++ RecApp_2/Models/Tratamiento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RecApp_2.Models
+{
+    public class Tratamiento
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido*")]
+        [Display(Name = "Nombre")]
+        public string Nombre { set; get; }
+
+        [Required(ErrorMessage = "Campo requerido*")]
+        [Display(Name = "Descripción")]
+        public string Descripcion { set; get; }
+
+
+        [DisplayFormat(DataFormatString = "{0:C0}")]
+        [DataType(DataType.Currency)]
+        public decimal? PrecioBase { get; set; }
+    }
+}
