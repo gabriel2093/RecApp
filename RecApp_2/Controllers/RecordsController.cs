a0616cc9f9e92cafc2b5bb91c238c1112b56024d
Gabriel Jiménez <gjimenez.com@gmail.com>
2016-10-22 14:38:34 -0600

    Correcciones expediente, pendiente editar tratamientos por paciente

--- RecApp_2/Controllers/RecordsController.cs
+++ RecApp_2/Controllers/RecordsController.cs
@@ -19,9 +19,7 @@ namespace RecApp_2.Controllers
         // GET: Records
         public async Task<ActionResult> Index(string search)
         {
-
-            return View(db.Records.Where(x => x.Nombre.StartsWith(search) || x.Apellido1.StartsWith(search) || x.Apellido2.StartsWith(search) || x.Cedula.ToString() == search || search == null).ToList());
-            // return View(await db.Records.ToListAsync());
+            return   View(await db.Records.Where(x => x.Nombre.StartsWith(search) || x.Apellido1.StartsWith(search) || x.Apellido2.StartsWith(search) || x.Cedula.ToString() == search || search == null).ToListAsync());         
         }
 
         // GET: Records/Details/5
@@ -42,21 +40,7 @@ namespace RecApp_2.Controllers
             return View(record);
         }
 
-        private int CalculateAdult(int edad)
-        {
-            return edad >= 18 ? 1 : 0;
-        }
-
-        private int CalculateAge(DateTime fechaNacimiento)
-        {
-            DateTime now = DateTime.Today;
-            int age = now.Year - fechaNacimiento.Year;
-            if (fechaNacimiento > now.AddYears(-age)) age--;
-            return age;
-        }
-
-
-
+       
         // GET: Records/Create
         public ActionResult Create()
         {
@@ -95,7 +79,6 @@ namespace RecApp_2.Controllers
         }
 
         // GET: Records/Edit/5
-
         public async Task<ActionResult> Edit(int? id)
         {
 
@@ -108,7 +91,7 @@ namespace RecApp_2.Controllers
             {
                 return HttpNotFound();
             }
-            if (TempData["AgregoTratamiento"] ==null)
+            if (TempData["AgregoTratamiento"] == null)
             {
                 TempData["AgregoTratamiento"] = "false";
             }
@@ -117,7 +100,10 @@ namespace RecApp_2.Controllers
             {
                 TempData["MensajeErrorAgregarTratamiento"] = "false";
             }
+
             record.ListCivilStatus = db.Civil_Status.ToList();
+            ViewBag.CivilStatus = db.Civil_Status.SingleOrDefault(cs => cs.Id.Equals(record.IdEstadoCivil)).Descripcion;
+            TempData["MensajeErrorEditarExpediente"] = "true";            
             record.Edad = CalculateAge(record.FechaNacimiento);
             record.ListTratamientoPaciente = from tP in
                                            db.TratamientoPaciente.ToList()
@@ -133,10 +119,57 @@ namespace RecApp_2.Controllers
             }
 
             var tuple = new Tuple<Record, TratamientoPaciente>(record, tratamientoPaciente);
-            return View(tuple);
-            //  return View(record);
+            return View(tuple);           
         }
 
+        // POST: Records/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Prefix = "Item1", Include = "id,Nombre,Apellido1,Apellido2,Cedula,FechaNacimiento,Edad,MenorEdad,NombreEncargado,Apellido1Encargado,Apellido2Encargado,IdEstadoCivil,Domicilio,Telefono1,Telefono2,Profesion,Email,ContactoEmergencia,TratamientoMedico,Medicamento,Diabetes,Artritis,EnfermedadCardiaca,Hepatitis,FiebreReumatica,Ulcera,PresionAlta,PresionBaja,EnfermedadesNerviosas,OtrasEnfermedades,SangradoProlongado,Desmayos,IntervencionQuirurgica,Aspirina,Sulfas,Penicilina,AnomaliasAnestesia,Embarazo,Lactancia,Otros")] Record record)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(record).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                TempData["MessageRegistroModificado"] = "El Expediente fue modificado  correctamente.";               
+                return RedirectToAction("Edit", "Records", new { id = record.id });
+            }
+
+             ViewBag.MessageErrorEdit = "Debe verificar los datos del Expediente.";
+            if (TempData["AgregoTratamiento"] == null)
+            {
+                TempData["AgregoTratamiento"] = "false";
+            }
+
+            if (TempData["MensajeErrorAgregarTratamiento"] == null)
+            {
+                TempData["MensajeErrorAgregarTratamiento"] = "false";
+            }
+            record.ListCivilStatus = db.Civil_Status.ToList();
+           // ViewBag.CivilStatus = db.Civil_Status.SingleOrDefault(cs => cs.Id.Equals(record.IdEstadoCivil)).Descripcion;
+            TempData["MensajeErrorEditarExpediente"] = "true";         
+            record.Edad = CalculateAge(record.FechaNacimiento);
+            record.ListTratamientoPaciente = from tP in
+                                           db.TratamientoPaciente.ToList()
+                                             where tP.IdPaciente.Equals(record.id)
+                                             select tP;
+
+            TratamientoPaciente tratamientoPaciente = new TratamientoPaciente();
+            tratamientoPaciente.ListTratamiento = db.Tratamiento.ToList();
+            foreach (var item in db.TratamientoPaciente.ToList())
+            {
+                item.Tratamiento = tratamientoPaciente.ListTratamiento.ToList().SingleOrDefault(t => t.id.Equals(item.IdTratamiento)).Nombre;
+                item.NombrePaciente = record.Nombre + " " + record.Apellido1;
+            }
+
+            // var tuple = new Tuple<Record, TratamientoPaciente>(record, null);
+            var tuple = new Tuple<Record, TratamientoPaciente>(record, tratamientoPaciente);
+            //return View(tuple);
+            return PartialView(tuple);
+        }
+               
         // GET: Records/Edit/5
         [HttpGet]
         public ActionResult GetTratamientosDiente(int? idDiente, int? idPaciente_1)
@@ -276,27 +309,7 @@ namespace RecApp_2.Controllers
             return PartialView("PartialViewTratamientosPaciente", record.ListTratamientoPaciente);
 
         }
-
-      
-
-        // POST: Records/Edit/5
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Prefix = "Item1", Include = "id,Nombre,Apellido1,Apellido2,Cedula,FechaNacimiento,Edad,MenorEdad,NombreEncargado,Apellido1Encargado,Apellido2Encargado,IdEstadoCivil,Domicilio,Telefono1,Telefono2,Profesion,Email,ContactoEmergencia,TratamientoMedico,Medicamento,Diabetes,Artritis,EnfermedadCardiaca,Hepatitis,FiebreReumatica,Ulcera,PresionAlta,PresionBaja,EnfermedadesNerviosas,OtrasEnfermedades,SangradoProlongado,Desmayos,IntervencionQuirurgica,Aspirina,Sulfas,Penicilina,AnomaliasAnestesia,Embarazo,Lactancia,Otros")] Record record)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(record).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-
-            record.ListCivilStatus = db.Civil_Status.ToList();
-
-            return View(record);
-        }
+           
 
         // GET: Records/Delete/5
         public async Task<ActionResult> Delete(int? id)
@@ -341,6 +354,19 @@ namespace RecApp_2.Controllers
         {
             //check if any of the UserName matches the UserName specified in the Parameter using the ANY extension method.  
             return db.Records.Any(record => record.Cedula == cedula);
+        }
+
+        private int CalculateAdult(int edad)
+        {
+            return edad >= 18 ? 1 : 0;
+        }
+
+        private int CalculateAge(DateTime fechaNacimiento)
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - fechaNacimiento.Year;
+            if (fechaNacimiento > now.AddYears(-age)) age--;
+            return age;
         }
 
         protected override void Dispose(bool disposing)
